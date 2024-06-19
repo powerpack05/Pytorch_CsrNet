ee87659a5fc83ea967058afe25d65c344d935eb4
Farheen Syed <102013740+powerpack05@users.noreply.github.com>
2024-06-19 11:22:59 +0530

    Update train.py
    changed no of epochs to 50 in the train.py

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def main():
     args.momentum      = 0.95
     args.decay         = 5*1e-4
     args.start_epoch   = 0
-    args.epochs = 400
+    args.epochs = 50
     args.steps         = [-1,1,100,150]
     args.scales        = [1,1,1,1]
     args.workers = 4
